74d5bd69eb68a78f7883209579a25ab8e7af3fd5
supriyapradhan <supriya@gmail.in>
2026-05-11 12:33:09 +0530

    fixed server

--- task-manager/server/server.js
+++ task-manager/server/server.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Home Route
+app.get("/", (req, res) => {
+  res.send("Task Manager Backend Running");
+});
+
+// Task Routes
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+// Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
